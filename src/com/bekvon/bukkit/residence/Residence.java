9901a346b81739ac151f86182c79690ebe69d594
Gary Smoak <GSmoak@gmail.com>
2011-09-06 17:13:58 -0400

    Fix broken enter / leave message commands

--- src/com/bekvon/bukkit/residence/Residence.java
+++ src/com/bekvon/bukkit/residence/Residence.java
@@ -1523,15 +1523,8 @@ public class Residence extends JavaPlugin {
                     } else if (args[1].equals("leave")) {
                         res = rmanager.getByLoc(player.getLocation());
                         start = 2;
-                    } else if (args.length>2 && args[2].equals("enter")) {
-                        enter = true;
-                        res = rmanager.getByName(args[1]);
-                        start = 3;
-                    } else if (args.length>2 && args[2].equals("leave")) {
-                        res = rmanager.getByName(args[1]);
-                        start = 3;
-                    } else if (args.length>2 && args[1].equals("remove")) {
-                        if (args[2].equals("enter")) {
+                    } else if (args[1].equals("remove")) {
+                        if (args.length>2 && args[2].equals("enter")) {
                             res = rmanager.getByLoc(player.getLocation());
                             if (res != null) {
                                 res.setEnterLeaveMessage(player, null, true, resadmin);
@@ -1539,7 +1532,7 @@ public class Residence extends JavaPlugin {
                                 player.sendMessage("§c"+language.getPhrase("InvalidResidence"));
                             }
                             return true;
-                        } else if (args[2].equals("leave")) {
+                        } else if (args.length>2 && args[2].equals("leave")) {
                             res = rmanager.getByLoc(player.getLocation());
                             if (res != null) {
                                 res.setEnterLeaveMessage(player, null, false, resadmin);
@@ -1550,7 +1543,15 @@ public class Residence extends JavaPlugin {
                         }
                         player.sendMessage("§c"+language.getPhrase("InvalidMessageType"));
                         return true;
-                    } else if (args.length>2 && args[2].equals("remove")) {
+                    } else if (args.length>2 && args[2].equals("enter")) {
+                        enter = true;
+                        res = rmanager.getByName(args[1]);
+                        start = 3;
+                    } else if (args.length>2 && args[2].equals("leave")) {
+                        res = rmanager.getByName(args[1]);
+                        start = 3;
+                    }
+                    else if (args.length>2 && args[2].equals("remove")) {
                         res = rmanager.getByName(args[1]);
                         if (args.length != 4) {
                             return false;
@@ -1581,7 +1582,7 @@ public class Residence extends JavaPlugin {
                     if (res != null) {
                         res.setEnterLeaveMessage(player, message, enter, resadmin);
                     } else {
-                        player.sendMessage("§c"+language.getPhrase("InvalidArea"));
+                        player.sendMessage("§c"+language.getPhrase("InvalidResidence"));
                     }
                     return true;
                 }
