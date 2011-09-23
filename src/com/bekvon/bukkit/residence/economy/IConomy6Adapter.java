6d217b19abf9c2447184844583e92b743993c504
Gary Smoak <GSmoak@gmail.com>
2011-09-23 15:46:41 -0400

    Add Iconomy 6 support

--- src/com/bekvon/bukkit/residence/economy/IConomy6Adapter.java
+++ src/com/bekvon/bukkit/residence/economy/IConomy6Adapter.java
@@ -5,6 +5,8 @@
 
 package com.bekvon.bukkit.residence.economy;
 import com.iCo6.iConomy;
+import com.iCo6.system.Account;
+import com.iCo6.system.Accounts;
 /**
  *
  * @author Administrator
@@ -21,27 +23,44 @@ public class IConomy6Adapter extends EconomyInterface {
 
     @Override
     public double getBalance(String playerName) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return new Accounts().get(playerName).getHoldings().getBalance();
     }
 
     @Override
     public boolean canAfford(String playerName, double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        double holdings = this.getBalance(playerName);
+        if(holdings>=amount)
+            return true;
+        return false;
     }
 
     @Override
     public boolean add(String playerName, double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        new Accounts().get(playerName).getHoldings().add(amount);
+        return true;
     }
 
     @Override
     public boolean subtract(String playerName, double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(this.canAfford(playerName, amount))
+        {
+            new Accounts().get(playerName).getHoldings().subtract(amount);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean transfer(String playerFrom, String playerTo, double amount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(this.canAfford(playerFrom, amount))
+        {
+            Account p1 = new Accounts().get(playerFrom);
+            Account p2 = new Accounts().get(playerTo);
+            p1.getHoldings().subtract(amount);
+            p2.getHoldings().add(amount);
+            return true;
+        }
+        return false;
     }
 
 }
