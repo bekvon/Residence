ca0cc271a06c92512fde46d641384110eeef0e2e
inorixu <inorinokokoro@gmail.com>
2012-03-27 00:07:59 +0800

    add event for repeat call event

--- src/com/inori/utils/ILog.java
+++ src/com/inori/utils/ILog.java
@@ -1,8 +1,10 @@
 package com.inori.utils;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 import com.bekvon.bukkit.residence.Residence;
 
@@ -16,13 +18,31 @@ public class ILog {
 	
 	private static Logger logger;
 	private static String PREFIX = "[Residence] ";
-	private static Boolean enable = false;
+	private static Boolean enable = true;
 	
 	static {
 		logger = Residence.getServ().getLogger();
 		//logger.setLevel(Level.INFO);
 	}
-	
+
+	private static HashMap<PlayerMoveEvent, Integer> counter = new HashMap<PlayerMoveEvent, Integer>();
+	public static void repeatCallEvent(PlayerMoveEvent event) {
+		if(counter.containsKey(event))
+    	{
+    		Integer i = counter.get(event);
+    		i++;
+    		if(i < 100){
+    			counter.put(event, i);
+                Residence.getServ().getPluginManager().callEvent(event);    			
+    		}
+    		else
+    			return;
+    	}else{
+			counter.put(event, 0);
+            Residence.getServ().getPluginManager().callEvent(event);  
+    	}
+	}
+    
 	public static void log(String msg)
 	{
 		if(enable)
