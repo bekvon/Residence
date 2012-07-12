b73da8d084e66693b239ccbf8d2700c2dd7522c1
t00thpick1 <Danielfranchy@hotmail.com>
2012-07-11 21:03:18 -0300

    Added new mobs to animals flag

--- src/com/bekvon/bukkit/residence/listeners/ResidenceEntityListener.java
+++ src/com/bekvon/bukkit/residence/listeners/ResidenceEntityListener.java
@@ -36,8 +36,11 @@ import org.bukkit.entity.Cow;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.IronGolem;
+import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Pig;
 import org.bukkit.entity.Sheep;
+import org.bukkit.entity.Snowman;
 import org.bukkit.entity.Squid;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.Wolf;
@@ -119,7 +122,7 @@ public class ResidenceEntityListener implements Listener {
         Entity ent = event.getEntity();
         if(perms!=null)
         {
-            if(ent instanceof Pig || ent instanceof Sheep || ent instanceof Chicken || ent instanceof Wolf || ent instanceof Cow || ent instanceof Squid || ent instanceof Villager)
+            if(ent instanceof Snowman || ent instanceof IronGolem || ent instanceof Ocelot || ent instanceof Pig || ent instanceof Sheep || ent instanceof Chicken || ent instanceof Wolf || ent instanceof Cow || ent instanceof Squid || ent instanceof Villager || ent instanceof )
             {
                 if(!perms.has("animals", true))
                 {
