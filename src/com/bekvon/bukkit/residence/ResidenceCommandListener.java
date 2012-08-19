5067d88be61164fd3bd1941a7f723628e056b192
Gary Smoak <GSmoak@gmail.com>
2012-08-19 00:12:56 -0400

    Add basic command listener structure for upcoming split off of commands from Residence.java

---
+++ src/com/bekvon/bukkit/residence/ResidenceCommandListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bekvon.bukkit.residence;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class ResidenceCommandListener extends Residence {
+
+    @Override
+    public boolean onCommand(CommandSender cs, Command cmnd, String label, String[] args) {
+        return true;
+    }    
+
+}
