10238337edec8d9065f21ec9bfa61642091641b6
Gary Smoak <GSmoak@gmail.com>
2012-08-24 15:50:42 -0400

    increse size of test residences created in performance tester.

--- src/com/bekvon/bukkit/residence/testing/PerformanceTester.java
+++ src/com/bekvon/bukkit/residence/testing/PerformanceTester.java
@@ -58,7 +58,7 @@ public class PerformanceTester {
                 int basex = r.nextInt();
                 int basey = r.nextInt(100);
                 int basez = r.nextInt();
-                res.addResidence("res"+i, "Test", new Location(w,basex,basey,basez), new Location(w,basex+100,basey+100,basez+100));
+                res.addResidence("res"+i, "Test", new Location(w,basex,basey,basez), new Location(w,basex+1000,basey+100,basez+1000));
             }
         }
         System.out.println("Done..." + (System.currentTimeMillis() - start) + "ms");
