ada226581d6f1d145e7c96d14e3552c3967e89c0
t00thpick1 <t00thpick1dirko@gmail.com>
2012-10-30 19:49:36 -0300

    Beacon should be use not container part 1

--- src/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener.java
+++ src/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener.java
@@ -143,7 +143,7 @@ public class ResidencePlayerListener implements Listener {
 	}
 
 	private boolean isCanUseEntity_RClickOnly(Material mat, Block block) {
-		return mat == Material.FLOWER_POT || mat == Material.COMMAND || mat == Material.ANVIL || mat == Material.CAKE_BLOCK || mat == Material.NOTE_BLOCK || mat == Material.DIODE || mat == Material.DIODE_BLOCK_OFF || mat == Material.DIODE_BLOCK_ON || mat == Material.BED_BLOCK || mat == Material.WORKBENCH || mat == Material.BREWING_STAND || mat == Material.ENCHANTMENT_TABLE ||
+		return mat == Material.BEACON || mat == Material.FLOWER_POT || mat == Material.COMMAND || mat == Material.ANVIL || mat == Material.CAKE_BLOCK || mat == Material.NOTE_BLOCK || mat == Material.DIODE || mat == Material.DIODE_BLOCK_OFF || mat == Material.DIODE_BLOCK_ON || mat == Material.BED_BLOCK || mat == Material.WORKBENCH || mat == Material.BREWING_STAND || mat == Material.ENCHANTMENT_TABLE ||
 				Residence.getConfigManager().getCustomRightClick().contains(Integer.valueOf(block.getTypeId()));
 	}
 
