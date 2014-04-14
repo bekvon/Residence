e0dcd45ec206c0faa9ead626f1754e07a66c7c9d
t00thpick1 <t00thpick1dirko@gmail.com>
2014-04-14 16:44:43 -0400

    Healing false by default

--- src/main/java/net/t00thpick1/residence/protection/yaml/YAMLWorldArea.java
+++ src/main/java/net/t00thpick1/residence/protection/yaml/YAMLWorldArea.java
@@ -23,6 +23,7 @@ public class YAMLWorldArea implements WorldArea {
         this.world = world;
         if (!section.isConfigurationSection("Permissions")) {
             this.perms = section.createSection("Permissions");
+            setAreaFlag(FlagManager.HEALING, false);
         } else {
             this.perms = section.getConfigurationSection("Permissions");
         }
