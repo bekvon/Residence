0e813c4a2eaaa04d5f4c4407e6ebd03e15bdca0f
t00thpick1 <t00thpick1dirko@gmail.com>
2014-04-14 16:49:38 -0400

    Add more message tokens

--- src/main/java/net/t00thpick1/residence/listeners/StateAssurance.java
+++ src/main/java/net/t00thpick1/residence/listeners/StateAssurance.java
@@ -10,6 +10,7 @@ import net.t00thpick1.residence.api.areas.ResidenceArea;
 import net.t00thpick1.residence.api.areas.WorldArea;
 import net.t00thpick1.residence.api.events.PlayerChangedAreaEvent;
 import net.t00thpick1.residence.api.flags.FlagManager;
+import net.t00thpick1.residence.locale.LocaleLoader;
 import net.t00thpick1.residence.utils.Utilities;
 
 import org.bukkit.ChatColor;
@@ -95,7 +96,7 @@ public class StateAssurance implements Listener {
                 String leave = oldRes.getLeaveMessage();
 
                 if (messages && leave != null && !leave.equals("")) {
-                    player.sendMessage(formatString(leave, oldRes.getName(), player));
+                    player.sendMessage(formatString(leave, oldRes, player));
                 }
             }
             PlayerChangedAreaEvent event = new PlayerChangedAreaEvent(oldArea, newArea, player);
@@ -111,21 +112,25 @@ public class StateAssurance implements Listener {
                 String leaveMessage = oldRes.getLeaveMessage();
 
                 if (messages && leaveMessage != null && !leaveMessage.equals("") && !oldArea.equals(newRes.getTopParent())) {
-                    player.sendMessage(formatString(leaveMessage, oldRes.getName(), player));
+                    player.sendMessage(formatString(leaveMessage, oldRes, player));
                 }
             }
             String enterMessage = newRes.getEnterMessage();
 
             if (messages  && enterMessage != null && !enterMessage.equals("") && (oldRes == null || !newArea.equals(oldRes.getTopParent()))) {
-                 player.sendMessage(formatString(enterMessage, newRes.getName(), player));
+                 player.sendMessage(formatString(enterMessage, newRes, player));
             }
             PlayerChangedAreaEvent event = new PlayerChangedAreaEvent(oldArea, newArea, player);
             Residence.getInstance().getServer().getPluginManager().callEvent(event);
         }
     }
 
-    private static String formatString(String message, String areaName, Player player) {
-        return ChatColor.translateAlternateColorCodes('&', message.replaceAll("(%player%)", player.getName()).replaceAll("(%area%)", areaName.replaceAll("(_)", " ")));
+    private static String formatString(String message, ResidenceArea res, Player player) {
+        message = message.replaceAll("(%renter%)", res.isRented() ? res.getRenter() : LocaleLoader.getString("Info.Unrented"));
+        message = message.replaceAll("(%player%)", player.getName());
+        message = message.replaceAll("(%area%)", res.getName().replaceAll("(_)", " "));
+        message = message.replaceAll("(%owner%)", res.getOwner());
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 
     private static StateAssurance instance;
