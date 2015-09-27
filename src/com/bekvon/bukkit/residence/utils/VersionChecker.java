b2c1fcc8d399682d05a7e560702bcd4034e584be
Zrips <zripas@gmail.com>
2015-09-27 13:31:19 +0300

    Changed version checker to check on spigot page instead off github

--- src/com/bekvon/bukkit/residence/utils/VersionChecker.java
+++ src/com/bekvon/bukkit/residence/utils/VersionChecker.java
@@ -1,59 +1,63 @@
 package com.bekvon.bukkit.residence.utils;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import com.bekvon.bukkit.residence.Residence;
 
 public class VersionChecker {
     Residence plugin;
+    private int resource = 11480;
 
     public VersionChecker(Residence plugin) {
 	this.plugin = plugin;
     }
 
     public void VersionCheck(final Player player) {
-	if (!Residence.getConfigManager().versionCheck()) {
+	if (!Residence.getConfigManager().versionCheck())
 	    return;
-	}
 
 	Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
 	    public void run() {
-		String readURL = "https://raw.githubusercontent.com/Zrips/Residence/master/src/plugin.yml";
-		FileConfiguration config;
 		String currentVersion = plugin.getDescription().getVersion();
-		try {
-		    URL url = new URL(readURL);
-		    BufferedReader br = new BufferedReader(new InputStreamReader(
-			url.openStream()));
-		    config = YamlConfiguration.loadConfiguration(br);
-		    String newVersion = config.getString("version");
-		    br.close();
-		    if (!newVersion.equals(currentVersion)) {
-			String msg = ChatColor.GREEN + "***********************************************************\n" +
-			    ChatColor.GREEN + "* Residence " + newVersion + " is now available!\n" +
-			    "* Your version: " + currentVersion + "\n" +
-			    "* You can download new version from \n* " + ChatColor.BLUE + plugin.getDescription().getWebsite() +
-			    ChatColor.GREEN + "\n***********************************************************";
-			if (player != null) {
-			    player.sendMessage(msg);
-			} else {
-			    plugin.consoleMessage(msg);
-			}
-		    }
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
+		String newVersion = getNewVersion();
+		if (newVersion == null || !newVersion.equals(currentVersion))
+		    return;
+		List<String> msg = Arrays.asList(
+		    ChatColor.GREEN + "*********************** " + plugin.getDescription().getName() + " **************************",
+		    ChatColor.GREEN + "* " + newVersion + " is now available! Your version: " + currentVersion,
+		    ChatColor.GREEN + "* " + ChatColor.DARK_GREEN + plugin.getDescription().getWebsite(),
+		    ChatColor.GREEN + "************************************************************");
+		for (String one : msg)
+		    if (player != null)
+			player.sendMessage(one);
+		    else
+			plugin.consoleMessage(one);
 	    }
 	});
+    }
+
+    public String getNewVersion() {
+	try {
+	    HttpURLConnection con = (HttpURLConnection) new URL("http://www.spigotmc.org/api/general.php").openConnection();
+	    con.setDoOutput(true);
+	    con.setRequestMethod("POST");
+	    con.getOutputStream().write(("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=" + resource).getBytes("UTF-8"));
+	    String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
+	    if (version.length() <= 7)
+		return version;
+	} catch (Exception ex) {
+	    plugin.consoleMessage(ChatColor.RED + "Failed to check for " + plugin.getDescription().getName() + " update on spigot web page.");
+	}
+	return null;
     }
 
 }
