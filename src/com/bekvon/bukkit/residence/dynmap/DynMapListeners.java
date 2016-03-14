5401d75f3e1685ba7100b4a60a888987aafce766
Zrips <zripas@gmail.com>
2016-03-14 18:57:02 +0200

    Cleanup

--- src/com/bekvon/bukkit/residence/dynmap/DynMapListeners.java
+++ src/com/bekvon/bukkit/residence/dynmap/DynMapListeners.java
@@ -14,65 +14,51 @@ import com.bekvon.bukkit.residence.event.ResidenceRenameEvent;
 import com.bekvon.bukkit.residence.event.ResidenceRentEvent;
 import com.bekvon.bukkit.residence.event.ResidenceSizeChangeEvent;
 import com.bekvon.bukkit.residence.event.ResidenceSubzoneCreationEvent;
-import com.bekvon.bukkit.residence.utils.Debug;
 
 public class DynMapListeners implements Listener {
-//    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-//    public void onResidenceCreate(ResidenceCreationEvent event) {
-//	Residence.getDynManager().fireUpdate(event.getResidence(), event.getResidence().getSubzoneDeep());
-//    }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceAreaAdd(ResidenceAreaAddEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateAdd(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceAreaDelete(ResidenceAreaDeleteEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateRemove(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceSubZoneCreate(ResidenceSubzoneCreationEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateAdd(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceFlagChange(ResidenceFlagChangeEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateAdd(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceDelete(ResidenceDeleteEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateRemove(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceOwnerChange(ResidenceOwnerChangeEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateAdd(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceRename(ResidenceRenameEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateAdd(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceRent(ResidenceRentEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateAdd(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onResidenceSizeChange(ResidenceSizeChangeEvent event) {
-	Debug.D("Event : " + event.getEventName());
 	Residence.getDynManager().fireUpdateAdd(event.getResidence(), event.getResidence().getSubzoneDeep());
     }
 }
