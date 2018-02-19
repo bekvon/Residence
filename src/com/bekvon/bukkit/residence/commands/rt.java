f2ccfdcd582ae13f0d914ceed5ed7094756fde4b
Zrips <zripas@gmail.com>
2018-02-19 11:36:38 +0200

    New permission residence.randomtp.cooldownbypass

--- src/com/bekvon/bukkit/residence/commands/rt.java
+++ src/com/bekvon/bukkit/residence/commands/rt.java
@@ -85,7 +85,7 @@ public class rt implements cmd {
 	    return false;
 
 	int sec = plugin.getConfigManager().getrtCooldown();
-	if (plugin.getRandomTeleportMap().containsKey(tPlayer.getName()) && !resadmin) {
+	if (plugin.getRandomTeleportMap().containsKey(tPlayer.getName()) && !resadmin && !plugin.hasPermission(sender, "residence.randomtp.cooldownbypass", false)) {
 	    if (plugin.getRandomTeleportMap().get(tPlayer.getName()) + (sec * 1000) > System.currentTimeMillis()) {
 		int left = (int) (sec - ((System.currentTimeMillis() - plugin.getRandomTeleportMap().get(tPlayer.getName())) / 1000));
 		plugin.msg(tPlayer, lm.RandomTeleport_TpLimit, left);
