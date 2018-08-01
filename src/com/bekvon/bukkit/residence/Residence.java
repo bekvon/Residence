4ba06153fe5b38f6b1ea36119d9e84a3a4d6abdc
Zrips <zripas@gmail.com>
2018-08-01 10:52:29 +0300

    resadmin should be back

--- src/com/bekvon/bukkit/residence/Residence.java
+++ src/com/bekvon/bukkit/residence/Residence.java
@@ -485,12 +485,12 @@ public class Residence extends JavaPlugin {
 	    }
 
 	    this.getCommand("res").setExecutor(getCommandManager());
-//	    this.getCommand("resadmin").setExecutor(getCommandManager());
-//	    this.getCommand("residence").setExecutor(getCommandManager());
+	    this.getCommand("resadmin").setExecutor(getCommandManager());
+	    this.getCommand("residence").setExecutor(getCommandManager());
 
 	    this.getCommand("res").setTabCompleter(new TabComplete());
-//	    this.getCommand("resadmin").setTabCompleter(new TabComplete());
-//	    this.getCommand("residence").setTabCompleter(new TabComplete());
+	    this.getCommand("resadmin").setTabCompleter(new TabComplete());
+	    this.getCommand("residence").setTabCompleter(new TabComplete());
 
 //	    Residence.getConfigManager().UpdateConfigFile();
 
