16be4fd43d8078cf1a18d69826cd6debd7ac04bc
Zrips <zripas@gmail.com>
2020-07-13 15:36:57 +0300

    Fixing pset command not working from console

--- src/com/bekvon/bukkit/residence/commands/pset.java
+++ src/com/bekvon/bukkit/residence/commands/pset.java
@@ -18,7 +18,8 @@ public class pset implements cmd {
     @Override
     @CommandAnnotation(simple = true, priority = 800)
     public Boolean perform(Residence plugin, CommandSender sender, String[] args, boolean resadmin) {
-	if (!(sender instanceof Player) && args.length != 5 && args.length == 4 && !args[3].equalsIgnoreCase("removeall"))
+
+	if (!(sender instanceof Player) && args.length != 4 && args.length == 3 && !args[2].equalsIgnoreCase("removeall"))
 	    return false;
 
 	if (args.length == 2 && args[1].equalsIgnoreCase("removeall")) {
