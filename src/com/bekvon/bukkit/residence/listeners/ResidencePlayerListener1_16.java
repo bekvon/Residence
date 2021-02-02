8d7ded67b7aafe69c02005782ab794173aada2e5
BuildTools <unconfigured@null.spigotmc.org>
2021-02-02 17:05:17 +0200

    Too many respawn anchor checks

--- src/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener1_16.java
+++ src/com/bekvon/bukkit/residence/listeners/ResidencePlayerListener1_16.java
@@ -1,18 +1,14 @@
 package com.bekvon.bukkit.residence.listeners;
 
 import org.bukkit.Material;
-import org.bukkit.World.Environment;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.type.RespawnAnchor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 
-import com.bekvon.bukkit.cmiLib.CMIMaterial;
 import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.Flags;
 import com.bekvon.bukkit.residence.containers.lm;
@@ -50,9 +46,6 @@ public class ResidencePlayerListener1_16 implements Listener {
 	if (!mat.equals(Material.RESPAWN_ANCHOR))
 	    return;
 
-	ItemStack iih = event.getItem();
-	CMIMaterial heldItem = CMIMaterial.get(iih);
-
 	ClaimedResidence res = plugin.getResidenceManager().getByLoc(block.getLocation());
 	if (res == null)
 	    return;
@@ -60,18 +53,6 @@ public class ResidencePlayerListener1_16 implements Listener {
 	if (!res.isOwner(player) && !res.getPermissions().playerHas(player, Flags.anchor, FlagCombo.OnlyTrue)) {
 	    plugin.msg(player, lm.Residence_FlagDeny, Flags.anchor, res.getName());
 	    event.setCancelled(true);
-	    return;
 	}
-
-	if (!heldItem.equals(CMIMaterial.GLOWSTONE) && !player.getWorld().getEnvironment().equals(Environment.NETHER) && !res.getPermissions().has(Flags.explode, FlagCombo.TrueOrNone)) {
-	    event.setCancelled(true);
-	    return;
-	}
-
-	RespawnAnchor anchor = (RespawnAnchor) block.getBlockData();
-	if (anchor.getMaximumCharges() == anchor.getCharges() && !res.getPermissions().has(Flags.explode, FlagCombo.TrueOrNone) && !player.getWorld().getEnvironment().equals(Environment.NETHER)) {
-	    event.setCancelled(true);
-	}
-
     }
 }
