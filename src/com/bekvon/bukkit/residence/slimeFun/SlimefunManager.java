177366d487f47c10a0ed131a3c4f56c557dbb6d1
Zrips <Zripas@gmail.com>
2022-02-04 15:41:30 +0200

    SlimeFun support fix

--- src/com/bekvon/bukkit/residence/slimeFun/SlimefunManager.java
+++ src/com/bekvon/bukkit/residence/slimeFun/SlimefunManager.java
@@ -1,11 +1,17 @@
 package com.bekvon.bukkit.residence.slimeFun;
 
+import java.lang.reflect.Method;
+import java.util.function.Function;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
 
 import com.bekvon.bukkit.residence.Residence;
 
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.ProtectionManager;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.ProtectionModule;
 
 public class SlimefunManager {
 
@@ -15,19 +21,30 @@ public class SlimefunManager {
 
     public static void register(Residence residence) {
 	Bukkit.getServer().getScheduler().runTaskTimer(residence, task -> {
+	    ++times;
+	    if (times >= TRIES) {
+		residence.consoleMessage("&cFailed to initialize SlimeFun support");
+		task.cancel();
+	    }
 
 	    ProtectionManager manager = Slimefun.getProtectionManager();
 
 	    // Wait for protectionManager to load (loaded on first server tick)
 	    if (manager != null) {
-		manager.registerModule(Bukkit.getServer(), "Residence", plugin -> new SlimeFunResidenceModule(plugin));
-		residence.consoleMessage("Enabled compatability with SlimeFun plugin");
-		task.cancel();
-	    }
-
-	    if (++times >= TRIES) {
-		residence.consoleMessage("&cFailed to initialize SlimeFun support");
-		task.cancel();
+		try {
+		    Method method = manager.getClass().getMethod("registerModule", Server.class, String.class, Function.class);
+		    if (method != null) {
+			Function<Plugin, ProtectionModule> m = plugin -> new SlimeFunResidenceModule(plugin);
+			method.invoke(manager, Bukkit.getServer(), "Residence", m);
+			residence.consoleMessage("Enabled compatability with SlimeFun plugin");
+			task.cancel();
+		    }
+		} catch (Throwable e) {
+		    // Going with latest dev build supported approach
+		    manager.registerModule(Bukkit.getServer().getPluginManager(), "Residence", plugin -> new SlimeFunResidenceModule(plugin));
+		    residence.consoleMessage("Enabled compatability with SlimeFun plugin");
+		    task.cancel();
+		}
 	    }
 	}, 20, 20);
     }
